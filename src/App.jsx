daefaf597452da425cf2e2e76f278cab5a3baf34
Guillaume Bzh <guillaume4cebil@gmail.com>
2026-03-16 14:39:44 +0100

    Initial commit Kasa (react + vite + scss)

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Kasa</h1>
+      <p>Point de départ du projet.</p>
+    </div>
+  );
+}
+
+export default App;
