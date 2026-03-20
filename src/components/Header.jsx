3cd20f58122328a75500544489f22e576786f6d0
Guillaume Bzh <guillaume4cebil@gmail.com>
2026-03-20 14:39:20 +0100

    style (responsiv All) add responsiv tablet

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
         <header>
-            <img id="logo" src={logo} alt="Kasa" />
+            <img id="logoHeader" src={logo} alt="Kasa" />
             <ul className="nav">
                 <li className="nav__accueil"><Link to={'/'}>Accueil</Link></li>
                 <li className="nav__apropos"><Link to={'/about'}>À propos</Link></li>
