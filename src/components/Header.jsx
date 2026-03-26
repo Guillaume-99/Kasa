b6597ac619dac1cac51d3cbec464509a86c179a7
Guillaume Bzh <guillaume4cebil@gmail.com>
2026-03-26 12:18:21 +0100

    style (head accordion) change rotate accordion and add NavLink

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 import logo from '../assets/images/kasa_logo_pink.png'
 import '../assets/scss/header.scss'
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
     return (
         <header>
             <img id="logoHeader" src={logo} alt="Kasa" />
             <ul className="nav">
-                <li className="nav__accueil"><Link to={'/'}>Accueil</Link></li>
-                <li className="nav__apropos"><Link to={'/about'}>À propos</Link></li>
+                <li className="nav__accueil"><NavLink to={'/'} className={'nav-link'}>Accueil</NavLink></li>
+                <li className="nav__apropos"><NavLink to={'/about'} className={'nav-link'}>À propos</NavLink></li>
             </ul>
         </header>
     );
